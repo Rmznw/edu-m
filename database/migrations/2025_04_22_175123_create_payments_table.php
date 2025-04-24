e098fba5363915e4a1511a05a7a2b7367246f453
Rmznw <Ramazan.bk77@icloud.com>
2025-04-25 04:58:15 +0500

    second commit

--- database/migrations/2025_04_22_175123_create_payments_table.php
+++ database/migrations/2025_04_22_175123_create_payments_table.php
@@ -4,20 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
+
             $table->id();
             $table->timestamps();
             $table->foreignId('enrollment_id')->index()->constrained()->cascadeOnDelete();
             $table->date('paid_date');
             $table->double('amount')->default(0);
+
+
         });
     }
-
-
 
 
     public function down(): void
