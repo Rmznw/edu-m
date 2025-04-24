6e83d741ef1962054eb3fdb8b029329cc46f2237
Rmznw <Ramazan.bk77@icloud.com>
2025-04-25 04:58:53 +0500

    second commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\Student;
 use App\Models\Teacher;
 use App\Models\User;
-use Illuminate\Database\Seeder;use Illuminate\Support\Facades\App;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,20 +15,18 @@ class DatabaseSeeder extends Seeder
     {
 
 
-
-
-
-$this->call(CategorySeeder::class);
-$this->call(CourseSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(CourseSeeder::class);
 
 
         Student::factory()->count(30)->create();
-
 
 
         Teacher::factory()->count(10)->create();
 
 
         User::factory()->count(10)->create();
+
+
     }
 }
